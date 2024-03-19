da80ea2654c068d5e9682ac6fcab63efe1397079
sabnam <sabnamhazari@gmail.com>
2024-03-19 21:16:30 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Landing from './Components/Landing';
 
 const App =()=>
 {
+  const[rupeesInput, setRupeesInput]=useState();4
+  const[country, setCountry]=useState(
+    [
+      {
+        title: "America",
+        num : 70
+      },
+      {
+        title: "Japan",
+        num : 50
+      },
+      {
+        title: "Uae",
+        num : 90
+      }
+    ]
+  )
+
   return(<div>
       <div className='app-name'>Currency Converter</div>
-      <Landing/>
+      <Landing setRupeesInput={setRupeesInput} country={country} rupeesInput={rupeesInput}/>
   </div>)
 }
 export default App;
--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -1,21 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import Photo from '../Components/Assets/photo.png';
 import Vector from '../Components/Assets/Vector.png';
 
-const Landing =()=>
+const Landing =({rupeesInput, setRupeesInput, country})=>
 {
+    const[outputCurrency, setOutputCurrency] = useState();
+
+   const handleDrop=(number)=>
+   {
+      setOutputCurrency(rupeesInput*number)
+      console.log(rupeesInput*number, outputCurrency)
+   }
+
     return(<div className="home">
         <div className="home-box">
             <div className="box-head">Convert to any currency from Indian Rupee</div>
             <div className="input-boxes">
                 <div className="input-rs">
-                    <input type="text" className="input-rupees"/>
+                    <input type="text" className="input-rupees" onChange={(event)=>setRupeesInput(event.target.value)}/>
                     <div className="rupees">RS</div>
                 </div>
                 <div className="vector"><img src={Vector} alt="vector" /></div>
                 <div className="converted-output">
-                    <div className="output-currency">output</div>
-                    <div className="drop-down"></div>
+                    <div className="output-currency">{outputCurrency}</div>
+                    <div className="drop-down">
+                        <select name="country" id="country"  className="drop-down" onChange={(e)=>{handleDrop(e.target.value)}} >
+                           {country.map((item)=>
+                           {
+                            return(<option value={item.num} >{item.title}</option>)
+                           })}
+                        </select>
+                    </div>
                 </div>
             </div>
             <div><img src={Photo} alt="photo" /></div>
